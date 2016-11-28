7ee0d4afa41294e880edf12c66b44d5ee78a2bdc
tourunen <olli.tourunen@csc.fi>
2016-11-28 09:56:26 +0200

    added download script and .gitignore

---
+++ notebooks/download.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+base="https://raw.githubusercontent.com/CSC-IT-Center-for-Science/machine-learning-scripts/master/notebooks"
+
+wget "$base/keras-test-setup.ipynb"
+wget "$base/keras-mnist-mlp.ipynb"
+wget "$base/keras-mnist-cnn.ipynb"
