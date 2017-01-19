2bb860bfbba78be68994ef064bb06909652f46b8
Markus Koskela <markus.koskela@csc.fi>
2017-01-19 14:36:50 +0200

    Add wget'ting of several notebooks and some images

--- notebooks/download.sh
+++ notebooks/download.sh
@@ -2,6 +2,22 @@
 
 base="https://raw.githubusercontent.com/CSC-IT-Center-for-Science/machine-learning-scripts/master/notebooks"
 
-wget "$base/keras-test-setup.ipynb"
-wget "$base/keras-mnist-mlp.ipynb"
-wget "$base/keras-mnist-cnn.ipynb"
+for i in keras-test-setup.ipynb \
+         keras-mnist-mlp.ipynb \
+         keras-mnist-cnn.ipynb \
+         sklearn-mnist-dt.ipynb \
+         sklearn-mnist-nn.ipynb \
+         sklearn-mnist-svm.ipynb \
+         sklearn-mnist-viz.ipynb
+do
+    wget "$base/$i"
+done
+
+mkdir imgs && cd imgs
+
+for i in 500px-KnnClassification.svg.png \
+         Svm_max_sep_hyperplane_with_margin_small.png \
+         dtree.png
+do
+    wget "$base/imgs/$i"
+done
