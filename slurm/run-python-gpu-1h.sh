890a36c7ccea3cceba5529e4fb4f7be4123249f6
Markus Koskela <markus.koskela@csc.fi>
2019-03-07 10:48:51 +0000

    Update to Python 3.6

--- slurm/run-python-gpu-1h.sh
+++ slurm/run-python-gpu-1h.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -N 1 -p gpu --gres=gpu:k80:1 -t 1:00:00 --mem=4G
+#SBATCH -N 1 -p gpu --gres=gpu:k80:1 -t 1:00:00 --mem=8G
 
+module load python-env/3.6.3-ml
 module list
 
 set -xv
@@ -9,6 +10,6 @@ date
 hostname
 nvidia-smi
 
-srun python3.5 $*
+srun python3.6 $*
 
 date
--- slurm/run-python-gputest.sh
+++ slurm/run-python-gputest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -N 1 -p gputest --gres=gpu:k80:1 -t 15 --mem=2G
+#SBATCH -N 1 -p gputest --gres=gpu:k80:1 -t 15 --mem=8G
 
+module load python-env/3.6.3-ml
 module list
 
 set -xv
@@ -9,6 +10,6 @@ date
 hostname
 nvidia-smi
 
-srun python3.5 $*
+srun python3.6 $*
 
 date
