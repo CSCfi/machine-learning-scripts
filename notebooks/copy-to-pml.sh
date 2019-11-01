2c7199735564e9edb51a0f399af838c6966f622b
Mats Sjöberg <mats.sjoberg@csc.fi>
2019-11-01 14:30:35 +0000

    Added copy-to-pml.sh script :-)

---
+++ notebooks/copy-to-pml.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PML_DIR=../../intro-to-ml
+
+if [ ! -d $PML_DIR ]
+then
+    echo "ERROR: could not find PML_DIR=$PML_DIR"
+    exit 1
+fi
+
+(cd $PML_DIR
+ GIT_STATUS=$(git status --porcelain)
+ if [ "$GIT_STATUS" != "" ]
+ then
+     echo "$PML_DIR not up-to-date with git!"
+     exit 1
+ fi
+ git pull
+)
+
+try_copy () {
+    FROM=$1
+    TO=${PML_DIR}/$2
+    ls -l $FROM
+    ls -l $TO
+    cp -iv $FROM $TO
+}
+
+try_copy sklearn-mnist-lc.ipynb Exercise-02.ipynb
+try_copy sklearn-mnist-svm.ipynb Exercise-05.ipynb
+try_copy tf2-mnist-mlp.ipynb Exercise-09.ipynb
