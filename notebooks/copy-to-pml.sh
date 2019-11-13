84b7383d27531c020a97c40bca1ddb9f362991f2
Markus Koskela <markus.koskela@csc.fi>
2019-11-13 13:01:56 +0200

    Add more things to copy, edit code a bit

--- notebooks/copy-to-pml.sh
+++ notebooks/copy-to-pml.sh
@@ -9,7 +9,7 @@ then
 fi
 
 (cd $PML_DIR
- GIT_STATUS=$(git status --porcelain)
+ GIT_STATUS=$(git status --porcelain -uno)
  if [ "$GIT_STATUS" != "" ]
  then
      echo "$PML_DIR not up-to-date with git!"
@@ -22,12 +22,19 @@ try_copy () {
     FROM=$1
     TO=${PML_DIR}/$2
     ls -l $FROM
-    ls -l $TO
+    if [ -f $TO ]
+    then
+        ls -l $TO
+    fi
     cp -iv $FROM $TO
 }
 
-try_copy sklearn-mnist-lc.ipynb Exercise-02.ipynb
-try_copy sklearn-mnist-svm.ipynb Exercise-05.ipynb
-try_copy tf2-mnist-mlp.ipynb Exercise-09.ipynb
-try_copy sklearn-mnist-nb.ipynb Extra-01.ipynb
+try_copy sklearn-mnist-lc.ipynb   Exercise-02.ipynb
+try_copy sklearn-mnist-svm.ipynb  Exercise-05.ipynb
+try_copy tf2-mnist-mlp.ipynb      Exercise-09.ipynb
+try_copy sklearn-mnist-dr.ipynb   Exercise-11.ipynb
+try_copy sklearn-mnist-viz.ipynb  Exercise-12.ipynb
+try_copy sklearn-mnist-cl.ipynb   Exercise-13.ipynb
+try_copy sklearn-mnist-ad.ipynb   Exercise-14.ipynb
+try_copy sklearn-mnist-nb.ipynb   Extra-01.ipynb
 try_copy sklearn-mnist-grid.ipynb Extra-02.ipynb
