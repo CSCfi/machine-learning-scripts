1cd1153cb9f7345dc714b8c1a558f0a69eb17297
Mats Sjöberg <mats.sjoberg@csc.fi>
2021-02-09 16:37:55 +0200

    Fixed run-all-pytorch.sh

--- examples/run-all-pytorch.sh
+++ examples/run-all-pytorch.sh
@@ -5,20 +5,15 @@ SBATCH_TEST="$SBATCH -A project_2003959 --partition=test -t 15"
 SCRIPT="run-pytorch.sh"
 SCRIPT_HVD="run-pytorch-hvd.sh"
 
-jid1a=$($SBATCH $SCRIPT pytorch_dvc_cnn_simple.py)
-jid1b=$($SBATCH --dependency=afterany:$jid1a $SCRIPT pytorch_dvc_cnn_simple.py --test)
+jid1=$($SBATCH $SCRIPT pytorch_dvc_cnn_simple.py)
 
-jid2a=$($SBATCH $SCRIPT pytorch_dvc_cnn_pretrained.py)
-jid2b=$($SBATCH --dependency=afterany:$jid2a $SCRIPT pytorch_dvc_cnn_pretrained.py --test)
+jid2=$($SBATCH $SCRIPT pytorch_dvc_cnn_pretrained.py)
 
-jid3a=$($SBATCH $SCRIPT pytorch_gtsrb_cnn_simple.py)
-jid3b=$($SBATCH --dependency=afterany:$jid3a $SCRIPT pytorch_gtsrb_cnn_simple.py --test)
+jid3=$($SBATCH $SCRIPT pytorch_gtsrb_cnn_simple.py)
 
-jid4a=$($SBATCH $SCRIPT pytorch_gtsrb_cnn_pretrained.py)
-jid4b=$($SBATCH --dependency=afterany:$jid4a $SCRIPT pytorch_gtsrb_cnn_pretrained.py --test)
+jid4=$($SBATCH $SCRIPT pytorch_gtsrb_cnn_pretrained.py)
 
-jid8a=$($SBATCH $SCRIPT_HVD pytorch_dvc_cnn_simple_hvd.py)
-jid8b=$($SBATCH --dependency=afterany:$jid8a $SCRIPT pytorch_dvc_cnn_simple_hvd.py --test)
+jid8=$($SBATCH $SCRIPT_HVD pytorch_dvc_cnn_simple_hvd.py)
 
 jid5=$($SBATCH $SCRIPT pytorch_20ng_cnn.py)
 
@@ -26,16 +21,16 @@ jid6=$($SBATCH $SCRIPT pytorch_20ng_rnn.py)
 
 jid7=$($SBATCH $SCRIPT pytorch_20ng_bert.py)
 
-jidx=$($SBATCH_TEST --dependency=afterany:$jid1b:$jid2b:$jid3b:$jid4b:$jid5:$jid6:$jid7:$jid8b --job-name="summary" <<EOF
+jidx=$($SBATCH_TEST --dependency=afterany:$jid1:$jid2:$jid3:$jid4:$jid5:$jid6:$jid7:$jid8 --job-name="summary" <<EOF
 #!/bin/bash
-echo "** pytorch_dvc_cnn ($jid1a,$jid2a -> $jid1b,$jid2b) **"
-grep -h -B 1 'Accuracy' --no-group-separator slurm-{$jid1b,$jid2b}.out
+echo "** pytorch_dvc_cnn ($jid1,$jid2) **"
+grep -h -B 1 'Accuracy' --no-group-separator slurm-{$jid1,$jid2}.out
 echo
-echo "** pytorch_dvc_cnn_hvd ($jid8a -> $jid8b) **"
-grep -h -B 1 'Accuracy' --no-group-separator slurm-{$jid8b}.out
+echo "** pytorch_dvc_cnn_hvd ($jid8) **"
+grep -h -B 1 'Accuracy' --no-group-separator slurm-{$jid8}.out
 echo
-echo "** pytorch_gtsrb_cnn ($jid3a,$jid4a -> $jid3b,$jid4b) **"
-grep -h -B 1 'Accuracy' --no-group-separator slurm-{$jid3b,$jid4b}.out
+echo "** pytorch_gtsrb_cnn ($jid3,$jid4) **"
+grep -h -B 1 'Accuracy' --no-group-separator slurm-{$jid3,$jid4}.out
 echo
 echo "** pytorch_20ng_cnn ($jid5) **"
 grep -A 1 'Test set' slurm-${jid5}.out
