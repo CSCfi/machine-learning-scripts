e275bee6c1b2fb72cc46c84716142c724abdc0ed
Markus Koskela <markus.koskela@csc.fi>
2021-12-21 17:57:34 +0200

    Fix all ipynb artifacts, now runs as a script

--- examples/tf2-dvc-vit.py
+++ examples/tf2-dvc-vit.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python
 # coding: utf-8
 
 # # Dogs-vs-cats classification with ViT
 # 
-# In this notebook, we'll finetune a [Vision Transformer](https://arxiv.org/abs/2010.11929) (ViT) to classify images of dogs from images of cats using TensorFlow 2 / Keras and HuggingFace's [Transformers](https://github.com/huggingface/transformers). 
+# In this notebook, we'll finetune a [Vision Transformer]
+# (https://arxiv.org/abs/2010.11929) (ViT) to classify images of dogs
+# from images of cats using TensorFlow 2 / Keras and HuggingFace's
+# [Transformers](https://github.com/huggingface/transformers).
 # 
 # **Note that using a GPU with this notebook is highly recommended.**
 # 
 # First, the needed imports.
-
-# In[ ]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 
 from transformers import ViTFeatureExtractor, TFViTForImageClassification
 from transformers.utils import check_min_version
@@ -20,10 +17,12 @@ from transformers import __version__ as transformers_version
 
 import tensorflow as tf
 from tensorflow.keras.utils import plot_model
+from tensorflow.keras.callbacks import TensorBoard
 from PIL import Image
 
-import os, sys
+import os, sys, datetime
 import pathlib
+from time import time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -36,31 +35,31 @@ print('Using TensorFlow version:', tf.__version__,
       'Keras version:', tf.keras.__version__,
       'Transformers version:', transformers_version)
 
-
 # ## Data
-# 
-# The training dataset consists of 2000 images of dogs and cats, split in half.  In addition, the validation set consists of 1000 images, and the test set of 22000 images.  Here are some random training images:
-# 
-# ![title](imgs/dvc.png)
+#
+# The training dataset consists of 2000 images of dogs and cats, split
+# in half.  In addition, the validation set consists of 1000 images.
 
-# In[ ]:
+if 'DATADIR' in os.environ:
+    DATADIR = os.environ['DATADIR']
+else:
+    DATADIR = "/scratch/project_2005299/data/"
 
+print('Using DATADIR', DATADIR)
+datapath = os.path.join(DATADIR, "dogs-vs-cats/train-2000/")
+assert os.path.exists(datapath), "Data not found at "+datapath
 
-datapath = "/media/data/dogs-vs-cats/train-2000/"
 nimages = {'train':2000, 'validation':1000, 'test':22000}
 
-
 # ### Image paths and labels
-
-# In[ ]:
-
 
 def get_paths(dataset):
     data_root = pathlib.Path(datapath+dataset)
     image_paths = list(data_root.glob('*/*'))
     image_paths = [str(path) for path in image_paths]
     image_count = len(image_paths)
-    assert image_count == nimages[dataset], "Found {} images, expected {}".format(image_count, nimages[dataset])
+    assert image_count == nimages[dataset], \
+        "Found {} images, expected {}".format(image_count, nimages[dataset])
     return image_paths
 
 image_paths = dict()
@@ -68,11 +67,9 @@ image_paths['train'] = get_paths('train')
 image_paths['validation'] = get_paths('validation')
 image_paths['test'] = get_paths('test')
 
-
-# In[ ]:
-
-
-label_names = sorted(item.name for item in pathlib.Path(datapath+'train').glob('*/') if item.is_dir())
+label_names = sorted(item.name for item in
+                     pathlib.Path(datapath+'train').glob('*/')
+                     if item.is_dir())
 label_to_index = dict((name, index) for index,name in enumerate(label_names))
 
 def get_labels(dataset):
@@ -84,13 +81,9 @@ image_labels['train'] = get_labels('train')
 image_labels['validation'] = get_labels('validation')
 image_labels['test'] = get_labels('test')
 
-
 # ### Data loading
 # 
 # We now define a function to load the images. 
-
-# In[ ]:
-
 
 def pil_loadimg(path: str):
     with open(path, "rb") as f:
@@ -103,57 +96,50 @@ def pil_loader(imglist: list):
         res.append(pil_loadimg(i))
     return res
 
-
-# Next we specify the pre-trained ViT model we are going to use. The model [`"google/vit-base-patch16-224"`](https://huggingface.co/google/vit-base-patch16-224) is pre-trained on ImageNet-21k (14 million images, 21,843 classes) at resolution 224x224, and fine-tuned on ImageNet 2012 (1 million images, 1,000 classes) at resolution 224x224. 
+# Next we specify the pre-trained ViT model we are going to use. The
+# model ["google/vit-base-patch16-224"]
+# (https://huggingface.co/google/vit-base-patch16-224) is pre-trained
+# on ImageNet-21k (14 million images, 21,843 classes) at resolution
+# 224x224, and fine-tuned on ImageNet 2012 (1 million images, 1,000
+# classes) at resolution 224x224.
 # 
-# We'll use a pre-trained ViT feature extractor that matches the ViT model to preprocess the input images. 
-
-# In[ ]:
-
+# We'll use a pre-trained ViT feature extractor that matches the ViT
+# model to preprocess the input images.
 
 VITMODEL = 'google/vit-base-patch16-224'
 
 feature_extractor = ViTFeatureExtractor.from_pretrained(VITMODEL)
 
+# We load and process the training and validation images:
 
-# We load and preprocess the training and validation images:
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', '\ninputs_train = feature_extractor(images=pil_loader(image_paths[\'train\']),\n                                 return_tensors="tf")\ninputs_validation = feature_extractor(images=pil_loader(image_paths[\'validation\']),\n                                      return_tensors="tf")')
-
+print('Loading and processing training and validation images')
+t0 = time()
+inputs_train = feature_extractor(
+    images=pil_loader(image_paths['train']), return_tensors="tf")
+inputs_validation = feature_extractor(
+    images=pil_loader(image_paths['validation']), return_tensors="tf")
+print('Done in {:.2f} seconds'.format(time()-t0))
 
 # ### TF Datasets
 # 
 # Let's now define our TF `Dataset`s for training and validation data. 
 
-# In[ ]:
-
-
 BATCH_SIZE = 32
 
-dataset_train = tf.data.Dataset.from_tensor_slices((inputs_train.data, image_labels['train']))
+dataset_train = tf.data.Dataset.from_tensor_slices((inputs_train.data,
+                                                    image_labels['train']))
 dataset_train = dataset_train.shuffle(len(dataset_train)).batch(BATCH_SIZE,
                                                                 drop_remainder=True)
 dataset_validation = tf.data.Dataset.from_tensor_slices((inputs_validation.data,
                                                          image_labels['validation']))
 dataset_validation = dataset_validation.batch(BATCH_SIZE, drop_remainder=True)
 
-
 # ## Model
 # 
 # ### Initialization
 
-# In[ ]:
-
-
 model = TFViTForImageClassification.from_pretrained(VITMODEL, num_labels=1,
                                                     ignore_mismatched_sizes=True)
-
-
-# In[ ]:
-
 
 LR = 1e-5
 
@@ -165,51 +151,34 @@ model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
 
 print(model.summary())
 
-
 # ### Learning
 
-# In[ ]:
+logdir = os.path.join(os.getcwd(), "logs",
+                      "dvc-vit-"+datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+print('TensorBoard log directory:', logdir)
+os.makedirs(logdir)
+callbacks = [TensorBoard(log_dir=logdir)]
 
+EPOCHS = 4
 
-get_ipython().run_cell_magic('time', '', '\nEPOCHS = 4\n\nhistory = model.fit(dataset_train,\n                    validation_data=dataset_validation,\n                    epochs=EPOCHS, verbose=2) #callbacks=callbacks)')
-
-
-# In[ ]:
-
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,3))
-
-ax1.plot(history.epoch,history.history['loss'], label='training')
-ax1.plot(history.epoch,history.history['val_loss'], label='validation')
-ax1.set_title('loss')
-ax1.set_xlabel('epoch')
-ax1.legend(loc='best')
-
-ax2.plot(history.epoch,history.history['accuracy'], label='training')
-ax2.plot(history.epoch,history.history['val_accuracy'], label='validation')
-ax2.set_title('accuracy')
-ax2.set_xlabel('epoch')
-ax2.legend(loc='best');
-
+history = model.fit(dataset_train, validation_data=dataset_validation,
+                    epochs=EPOCHS, verbose=2, callbacks=callbacks)
 
 # ### Inference
 # 
-# We now evaluate the model using the test set. First we'll load and preprocess the test images and define the TF `Dataset`. Due to memory issues, we limit the evaluation to 5000 images.
+# We now evaluate the model using the test set. First we'll load and
+# preprocess the test images and define the TF Dataset. Due to
+# memory issues, we limit the evaluation to N_test images.
 
-# In[ ]:
+N_test = 5000
+print('Loading and processing', N_test, 'test images')
+t0 = time()
+inputs_test = feature_extractor(images=pil_loader(image_paths['test'][:N_test]),
+                                return_tensors="tf")
+dataset_test = tf.data.Dataset.from_tensor_slices((inputs_test.data,
+                                                   image_labels['test'][:N_test]))
+dataset_test = dataset_test.batch(BATCH_SIZE, drop_remainder=True)
+print('Done in {:.2f} seconds'.format(time()-t0))
 
-
-get_ipython().run_cell_magic('time', '', '\ninputs_test = feature_extractor(images=pil_loader(image_paths[\'test\'][:5000]),\n                                return_tensors="tf")\ndataset_test = tf.data.Dataset.from_tensor_slices((inputs_test.data,\n                                                   image_labels[\'test\'][:5000]))\ndataset_test = dataset_test.batch(BATCH_SIZE, drop_remainder=True)')
-
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', '\nscores = model.evaluate(dataset_test, verbose=2)\nprint("Test set %s: %.2f%%" % (model.metrics_names[1], scores[1]*100))')
-
-
-# Alternatively, we could evaluate the model using the validation set
-%%time
-
-scores = model.evaluate(dataset_validation, verbose=2)
-print("Validation set %s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+scores = model.evaluate(dataset_test, verbose=2)
+print("Test set %s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
