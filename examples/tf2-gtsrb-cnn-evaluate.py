eb1cfd4f81d10809fb4743992de2add8f8ed871d
Markus Koskela <markus.koskela@csc.fi>
2021-12-21 14:38:34 +0200

    Switch to Keras augmentation, fix many other things

--- examples/tf2-gtsrb-cnn-evaluate.py
+++ examples/tf2-gtsrb-cnn-evaluate.py
@@ -1,9 +1,20 @@
-import os
-import sys
+# coding: utf-8
+
+# # Traffic sign classification with CNNs
+#
+# This script is used to evaluate neural networks trained using
+# TensorFlow 2 / Keras to classify images of traffic signs from The
+# German Traffic Sign Recognition Benchmark.
+# 
+# **Note that using a GPU with this notebook is highly recommended.**
+# 
+# First, the needed imports.
+
+import os, sys
 import pathlib
 
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from tensorflow import keras
 import numpy as np
 
 from PIL import Image
@@ -18,19 +29,13 @@ print('Using Tensorflow version: {}, and Keras version: {}.'.format(
 if 'DATADIR' in os.environ:
     DATADIR = os.environ['DATADIR']
 else:
-    DATADIR = "/scratch/project_2003747/data/"
+    DATADIR = "/scratch/project_2005299/data/"
 
+print('Using DATADIR', DATADIR)
 datapath = os.path.join(DATADIR, "gtsrb/train-5535/")
+assert os.path.exists(datapath), "Data not found at "+datapath
 
-nimages = dict()
-nimages['test'] = 12630
-
-# ### Parameters
-
-INPUT_IMAGE_SIZE = [75, 75, 3]
-BATCH_SIZE = 50
-NUM_CLASSES = 43
-
+nimages = {'test':12630}
 
 # ### Image paths and labels
 
@@ -43,7 +48,6 @@ def get_paths(dataset):
         "Found {} images, expected {}".format(image_count, nimages[dataset])
     return image_paths
 
-
 image_paths = dict()
 image_paths['test'] = get_paths('test')
 
@@ -52,60 +56,44 @@ label_names = sorted(item.name for item in
                      item.is_dir())
 label_to_index = dict((name, index) for index, name in enumerate(label_names))
 
-
 def get_labels(dataset):
     return [label_to_index[pathlib.Path(path).parent.name]
             for path in image_paths[dataset]]
 
-
 image_labels = dict()
 image_labels['test'] = get_labels('test')
 
+# ###Data loading
+#
+# We now define a function to load the images. The images are in PPM
+# format, so we use the PIL library. Also we need to resize the images
+# to a fixed size (INPUT_IMAGE_SIZE).
 
-# ### Data augmentation
+INPUT_IMAGE_SIZE = [80, 80]
 
 def _load_image(path, label):
     image = Image.open(path.numpy())
     return np.array(image), label
 
-
 def load_image(path, label):
-    return tf.py_function(_load_image, (path, label), (tf.float32, tf.int32))
-
-
-def preprocess_image(image, augment):
+    image, label = tf.py_function(_load_image, (path, label),
+                                  (tf.float32, tf.int32))
     image.set_shape([None, None, None])
-    if augment:
-        image = tf.image.resize(image, [80, 80])
-        image = tf.image.random_crop(image, INPUT_IMAGE_SIZE)
-        #image = tf.image.random_flip_left_right(image)
-        image = tf.image.random_brightness(image, 0.1)
-        image = tf.clip_by_value(image, 0.0, 255.0)
-    else:
-        image = tf.image.resize(image, INPUT_IMAGE_SIZE[:2])
-    image /= 255.0  # normalize to [0,1] range
-    image.set_shape(INPUT_IMAGE_SIZE)
-    return image
-
-
-def process_and_augment_image(image, label):
     label.set_shape([])
-    return preprocess_image(image, True), label
-
-
-def process_and_not_augment_image(image, label):
-    label.set_shape([])
-    return preprocess_image(image, False), label
-
+    return tf.image.resize(image, INPUT_IMAGE_SIZE), label
 
 # ### TF Datasets
+#
+# Let's now define our TF Datasets for the test data.
+
+BATCH_SIZE = 50
 
 test_dataset = tf.data.Dataset.from_tensor_slices(
     (image_paths['test'], image_labels['test']))
-test_dataset = test_dataset.map(load_image)
-test_dataset = test_dataset.map(process_and_not_augment_image,
-                                num_parallel_calls=10)
+test_dataset = test_dataset.map(load_image,
+                                num_parallel_calls=tf.data.AUTOTUNE)
 test_dataset = test_dataset.batch(BATCH_SIZE, drop_remainder=False)
+test_dataset = test_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 # ### Initialization
 
@@ -113,7 +101,7 @@ if len(sys.argv) < 2:
     print('ERROR: model file missing')
     sys.exit()
 
-model = load_model(sys.argv[1])
+model = keras.models.load_model(sys.argv[1])
 
 print(model.summary())
 
