5b3023e80303a6519ef8a45a164e4f2bc4537bc2
Markus Koskela <markus.koskela@csc.fi>
2023-10-10 15:07:35 +0300

    Add slurm run file for LUMI

---
+++ slurm/run-lumi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=<project>
+#SBATCH --partition=dev-g
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=7
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=60G
+#SBATCH --time=1:00:00
+
+srun python3 $*
