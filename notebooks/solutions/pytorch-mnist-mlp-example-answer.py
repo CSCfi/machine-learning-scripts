72fa37d05474405e5f9b66684fdc0994c5d5f397
Mats Sjöberg <mats.sjoberg@csc.fi>
2023-12-20 11:20:28 +0200

    New PyTorch versions for mnist-mlp and mnist-cnn

---
+++ notebooks/solutions/pytorch-mnist-mlp-example-answer.py
@@ -0,0 +1,15 @@
+class TwoLayerMLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28*28, 50),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(50, 50),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
