d8eb8163c6bd1457e1d75a6c6a40e528b0e6d6f3
Mats Sjöberg <mats.sjoberg@csc.fi>
2023-12-22 15:43:36 +0200

    Initial version of PyTorch IMDB RNN example

---
+++ notebooks/solutions/pytorch-mnist-rnn-example-answer.py
@@ -0,0 +1,19 @@
+embedding_dims = 50
+lstm_units = 32
+
+class TwoLayeredRNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.emb = nn.Embedding(nb_words, embedding_dims)
+        self.dropout = nn.Dropout(0.2)
+        self.lstm = nn.LSTM(embedding_dims, lstm_units, num_layers=2,
+                            batch_first=True)
+        self.linear = nn.Linear(lstm_units, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.emb(x)
+        x = self.dropout(x)
+        x, (hn, cn) = self.lstm(x)
+        x = self.linear(x[:, -1, :])
+        return self.sigmoid(x.view(-1))
